f90152c622f811fff1048725c4735277c53f87c9
Sleep1ngForest <143532490+Sleep1ngForest@users.noreply.github.com>
2023-10-01 23:12:42 +0300

    seminar-3

---
+++ task17/Program.cs
@@ -0,0 +1,32 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+Console.WriteLine("Введите координату x: ");
+int x =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится в первой четверти");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Точка находится во второй четверти");
+}
+
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится в третьей четверти");
+}
+
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("Точка находится в четвертой четверти");
+}
+
+else
+{
+     System.Console.WriteLine("Точка не может содержать координату 0");
+}
---
+++ task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNumbersToN(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i; // == sum = sum + 1
+    }
+    return sum;
+}
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = SumNumbersToN(N);
+System.Console.WriteLine($"Результат сложения от 1 до {N} = {result}");
