b4c8a6e59c2ad3ad1e5cb52ed0921691e0f9c9d4
Sleep1ngForest <143532490+Sleep1ngForest@users.noreply.github.com>
2023-10-01 23:14:23 +0300

    seminar-3

---
+++ task26/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+// 1 Вариант решения
+// Console.WriteLine("Введите число: ");
+// int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// int count = 0;
+// if (number == 0)
+// {
+//     Console.WriteLine(1);
+// }
+// else
+// {
+//     while (number > 0)
+//     {
+//         count++;
+//         number /= 10;
+//     }
+
+//     Console.WriteLine(count);
+// }
+
+//2 Вариант 
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string num = Convert.ToString(number); // 123 = "123"
+System.Console.WriteLine(num.Length); // ""
---
+++ task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+int n=8;
+int[] mass= new int[n];
+for (int i=0; i<n;i++){
+    mass[i]= new Random().Next(0,2);
+}
+
+for (int i=0; i<n;i++){
+    System.Console.Write($"{mass[i]},");
+}
+
+20:47
+int writeMult(int num){
+    int mult = 1;
+    for(int i=1; i<=num;i++){
+        mult*=i;
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(writeMult(number));
